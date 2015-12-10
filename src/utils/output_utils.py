aab9a90730d0e04168a017b7d94b9b393d4fa13e
tommycarpi <tommy@tommy.local>
2015-12-10 15:28:21 +0100

    fixes

--- src/utils/output_utils.py
+++ src/utils/output_utils.py
@@ -1,6 +1,6 @@
 # Write on file
 def create_sub_row(elem):
-    return str(elem[0]) + "\t" + str(elem[1]) + "\t" + str(elem[2]) + "\t" + str(elem[3]) + "\t" + str(elem[4])
+    return str(elem[0]) + "," + str(elem[1]) + "," + str(elem[2]) + "," + str(elem[3]) + "," + str(elem[4])
 
 def write_csv(interval_list, event_type, init_time, finish_time, granularity):
     f = open("../csv/jobs_intervals_"+str(event_type)+"_"+str(init_time)+"_"+str(finish_time)+"_@"+str(granularity),'w')
